1e34006f60c5f86b15f76d8ceff104eb5be6c6f4
Jamez <adobeclip2003@gmail.com>
2023-11-07 13:57:59 +0100

    Login Request Handler Completed

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const handleRequests = require('./views/handleReq')
+const middleware = require('./views/handleReq')
 const bcrypt = require('bcrypt')
 const { check, validationResult } = require('express-validator')
 const urlencodedParser = require('body-parser').urlencoded({ extended: false })
@@ -17,22 +17,27 @@ router.get('/login', (req, res) => {
     })
 })
 
-router.post('/handleReq', handleRequests, (req, res) => {
+router.post('/handle_requests_signup', middleware.handle_signup_requests, (req, res) => {
     let data = res.locals.data
     console.log(data);
-    if (data.page == 'signup' && data.msg != "success") res.render("signUp", {
+    if (data.msg != "success") res.render("signUp", {
         title: "Sign Up",
-        data
-    })
-
-    if (data.page == 'login' && data.msg != "success") res.render("login", {
-        title: "Login",
         data
     })
 
     // if(data.auto_login === 'undefined') // render login page
 })
 
+router.post('/handle_requests_login', middleware.handle_login_requests, (req, res) => {
+    let data = res.locals.data
+    console.log(data);
+    if (data.msg != "success") res.render("login", {
+        title: "Login",
+        data
+    })
+
+    // res.send('<h1>Welcome</h1>')
+})
 // router.post('/register', urlencodedParser, [
 //     check('username', 'This username must me 3+ characters long')
 //         .exists()
--- views/handleReq.js
+++ views/handleReq.js
@@ -21,46 +21,57 @@ function emailValid(mail) {
 
     return true
 }
-    
-let data;
-function setData(valid_name, valid_email, user_input, sourcePage, msgArray){
-    const auto_login = typeof user_input.auto_login === 'undefined' ? 'off' : 'on'
 
-    if (valid_name && valid_email) {
-        data = {
-            page : sourcePage,
-            msg: "success"
-        }
-    } else {
-        data = {
-            page : sourcePage,
-            msg: msgArray
-        }
-    }
-
-    if(sourcePage == 'signup') data.auto_login = auto_login;
-}
-function handleRequests(req, res, next) {
+let signupSrc;
+function handle_signup_requests(req, res, next) {
     let sourcePage = req.headers.referer.split("/").slice(-1)[0]
+    if(typeof signupSrc === 'undefined') signupSrc = req.headers.referer.split("/").slice(-1)[0]
     const user_input = req.body
     const valid_name = nameValid(user_input.name)
     const valid_email = emailValid(user_input.email)
-    let msgArray = [];
+    let msgArray = [], data;
 
     if (!valid_name) msgArray.push("No numbers allowed in Username")
     if (!valid_email) msgArray.push("Invalid Email Extension")
 
-    // if there's an error caused by the try-block, then it means data contains nothing implying the user just visited the page. If there's no error caused by the try-block, then it means data already exists implying the user made an error and the page re-rendered prompting a login again with valid inputs
-    try{
-        console.log("Server rendering : " + data.page + " page");
-        sourcePage = data.page
-        setData(valid_name, valid_email, user_input, sourcePage, msgArray)
-    }catch(error){
-        setData(valid_name, valid_email, user_input, sourcePage, msgArray)
+    const auto_login = typeof user_input.auto_login === 'undefined' ? 'off' : 'on'
+
+    if (valid_name && valid_email) {
+        data = {
+            page: signupSrc,
+            auto_login: auto_login,
+            msg: "success"
+        }
+    } else {
+        data = {
+            page: signupSrc,
+            msg: msgArray
+        }
     }
 
     res.locals.data = data
     next()
 }
 
-module.exports = handleRequests
+let loginSrc;
+function handle_login_requests(req, res, next) {
+    let msgArray = [], data;
+    if(typeof loginSrc === 'undefined') loginSrc = req.headers.referer.split("/").slice(-1)[0]
+    const valid_email = emailValid(req.body.email)
+
+    if (!valid_email) msgArray.push("Invalid Email Extension")
+
+    if (valid_email) data = {
+        page: loginSrc,
+        msg: "success"
+    }
+    else data = {
+        page: loginSrc,
+        msg: msgArray
+    }
+
+    res.locals.data = data
+    next()
+}
+
+module.exports = {handle_signup_requests, handle_login_requests}
