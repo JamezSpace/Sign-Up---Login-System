bc7b73a62dcded7df2fd0346db3b63d86e4a9746
Jamez <adobeclip2003@gmail.com>
2023-11-07 12:14:20 +0100

    Common Validations to both Sign Up and Login Page

--- views/signUp.js
+++ views/signUp.js
@@ -1,3 +1,4 @@
+import { validEmail, defaultInputs } from "./common.js";
 
 // fetch(`/handleReq`, {
 //     method: "GET"
@@ -16,18 +17,6 @@
 //     scanFor: "" ,
 // };
 
-class toDefault {
-    property;
-    default;
-    scanFor;
-
-    constructor(property, myDefault, scanFor) {
-        this.property = property;
-        this.default = myDefault;
-        this.scanFor = scanFor;
-    }
-}
-
 function validName(nm) {
     'use strict'
 
@@ -36,15 +25,6 @@ function validName(nm) {
 
     // test for full name
     if (nm.split(' ').length < 2) return false;
-
-    return true;
-}
-
-function validEmail(mail) {
-    'use strict'
-
-    // test for invalid email
-    if (mail.split('@')[0].length < 2) return false;
 
     return true;
 }
@@ -59,11 +39,11 @@ function validInputs() {
     const valid_password = [password_field.value.trim().length > 1 ? true : false, password_field];
     const all_inputs = [valid_name, valid_email, valid_password];
 
-    defaultInputs(new toDefault("borderColor", "rgb(0,0,0,0.5)", "red") , name_field, email_field, password_field);
+    defaultInputs(name_field, email_field, password_field);
 
     for (let i = 0; i < all_inputs.length; i++) {
         if (!all_inputs[i][0]) {
-            all_inputs[i][1].style.borderColor = "red";
+            all_inputs[i][1].classList.add("invalid");
             return false;
         }  
     }
@@ -71,30 +51,6 @@ function validInputs() {
     return true;
 }
 
-function defaultInputs(toDefault, ...elements) {
-    const property = toDefault.property;
-    const my_default = toDefault.default;
-    const checkFor = toDefault.scanFor; 
-
-    for (let i = 0; i < elements.length; i++) {
-        const each_input = elements[i];
-
-        if (eval(`each_input.style.${property}`) == checkFor) {
-            eval(`each_input.style.${property} = "${my_default}"`);
-        }
-    }
-}
-
 document.getElementById("signUp").addEventListener("submit", (e) => {
     if (!validInputs()) e.preventDefault();
 })
-
-const close_btns = document.getElementsByClassName("close_error_bar");
-
-for (let i = 0; i < close_btns.length; i++) {
-    const close_btn = close_btns[i];
-    close_btn.addEventListener("click", () => {
-        close_btn.parentNode.style.display = "none";
-    })
-    
-}
