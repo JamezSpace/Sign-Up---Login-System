ad87e566b82d2f64037709c54bbd44d7ace9f734
Jamez <adobeclip2003@gmail.com>
2023-11-07 17:24:43 +0100

    Added a temporary database (array of objects)

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const middleware = require('./views/handleReq')
 const bcrypt = require('bcrypt')
+const database = require('./database')
 const urlencodedParser = require('body-parser').urlencoded({ extended: false })
-
 
 router.get('/signup', (req, res) => {
     res.render("signUp", {
@@ -16,26 +16,46 @@ router.get('/login', (req, res) => {
     })
 })
 
-router.post('/handle_requests_signup', middleware.handle_signup_requests, (req, res) => {
+router.post('/handle_requests_signup', middleware.handle_signup_requests, async (req, res) => {
     let data = res.locals.data
-    console.log(data);
+    
     if (data.msg != "success") res.render("signUp", {
         title: "Sign Up",
         data
     })
 
-    // if(data.auto_login === 'undefined') // render login page
+    const hashedPassword = await bcrypt.hash(data.user_details.user_password, 10)
+    data.user_details.user_password = hashedPassword
+    const obj = new database()
+    obj.add_to_db = data
+
+    if(data.user_details.auto_login === 'off') res.redirect('/login')
 })
 
 router.post('/handle_requests_login', middleware.handle_login_requests, (req, res) => {
     let data = res.locals.data
-    console.log(data);
+    let dataObject = data.user_details
+
     if (data.msg != "success") res.render("login", {
         title: "Login",
         data
     })
+    
+    const dbObject = new database().checkIfExists(dataObject.user_email, dataObject.user_password)
 
-    // res.send('<h1>Welcome</h1>')
+    if (dbObject === 1) res.send('<h1>Welcome</h1>')
+    else if(dbObject === 0) res.render("login", {
+        title : "Login",
+        data : {
+            msg : ["Incorrect Password!!"]
+        }
+    })
+    else res.render("login", {
+        title : "Login",
+        data : {
+            msg : ["User does not exist"]
+        }
+    })
 })
 
 module.exports = router
